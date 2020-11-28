6c096f73ed546dc69fa1a9ac32cabeb9bedc19c3
minjeongkang <snowdrop1202@gmail.com>
2020-11-29 05:09:01 +0900

    CONSTANTS 임시 추가 * 지역명 매칭 영문/국문 임시 추가 * 24시간제가 아닌 12시간제로 변경할 떄 노출 텍스트 추가

--- scripts/weatherPrototype.js
+++ scripts/weatherPrototype.js
@@ -2,6 +2,17 @@
   'use strict';
   let utils = {};
 
+  const CITIES = {
+    '서울': 'seoul'
+  };
+
+  const TIME_TEXT = {
+    MORNING: '오전',
+    AFTERNOON: '오후'
+  }
+
+  const {MORNING, AFTERNOON} = TIME_TEXT;
+
   const API = {
     URL: 'https://api.openweathermap.org/data/2.5/weather',
     KEY: '&appid=8d0fba684264cc55f9b735566611ac78'
