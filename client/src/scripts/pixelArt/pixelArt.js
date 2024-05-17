607abbacd98c25520d12a02dda19f8460a927b38
JIWOO <snowdrop1202@gmail.com>
2024-05-17 18:18:24 +0900

    feat: pixelArt init

---
+++ client/src/scripts/pixelArt/pixelArt.js
@@ -0,0 +1,17 @@
+const prototype = (() => {
+    'use strict';
+
+    const init = () => {
+        console.log('init');
+    };
+
+    return {
+        init
+    }
+})();
+
+if (document.readyState === 'complete') {
+    prototype.init();
+} else if (document.addEventListener) {
+    document.addEventListener('DOMContentLoaded', prototype.init);
+}
