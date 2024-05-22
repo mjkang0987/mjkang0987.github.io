a0bef1cdd2c6b4efbd28cd28916d45191c19a026
JIWOO <snowdrop1202@gmail.com>
2024-05-22 14:43:43 +0900

    feat: add dynamic element creation script

--- client/src/scripts/pixelArt/pixelArt.js
+++ client/src/scripts/pixelArt/pixelArt.js
@@ -15,11 +15,33 @@ const {
 } = UTILS;
 
 function PixelArt() {
+    this.selector = {};
+
     this.init();
 }
 
 PixelArt.prototype = {
     init: function () {
         const input = document.createElement;
+        this.setElement();
+    },
+    insertElement : function (el, parent, position) {
+        const parentElement = this.selector[parent];
+        parentElement.insertAdjacentElement(position, el);
+    },
+    setElement    : function () {
+        for (let i = 0; i < LENGTH.ELEMENTS; i++) {
+            const current = ELEMENTS[i];
+            const element = CREATE_EL(current.el, current.attr);
+
+            this.selector[current.selector] = element;
+
+            if (current.parentSelector) {
+                this.insertElement(element, current.parentSelector, current.insertPosition);
+            }
+        }
+
+        SELECTOR.BODY.insertAdjacentElement('afterbegin', this.selector['PIXEL']);
     }
 };
+new PixelArt();
