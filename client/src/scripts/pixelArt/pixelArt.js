d4cdf13acbac00c04df655aa6069460ab8fcc6c0
JIWOO <snowdrop1202@gmail.com>
2024-05-22 14:41:51 +0900

    chore: remove comment

--- client/src/scripts/pixelArt/pixelArt.js
+++ client/src/scripts/pixelArt/pixelArt.js
@@ -14,13 +14,6 @@ const {
     CREATE_EL,
 } = UTILS;
 
-/*
- * Creates pixel art grid
- * @param el DOM Element
- * @param rows Number of rows
- * @param rows Number of cols
- */
-
 function PixelArt(el, rows, cols) {
     this.init();
 }
